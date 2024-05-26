3ecc3c87defd874e0318a59ec49f893f6f967db3
Tyuiu.MalsagovUA@outlook.com <mals.umar@yandex.ru>
2024-05-26 15:27:13 +0500

    Task6

---
+++ Tyuiu.UsoltsevAD.Sprint3.Task6.V13/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.UsoltsevAD.Sprint3.Task6.V13.Lib;
+namespace Tyuiu.UsoltsevAD.Sprint3.Task6.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int startValue = 8;
+            int stopValue = 17;
+            Console.Title = "Спринт #3 | Выполнил: Усольцев А.Д. | АСОиУБ-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнил: Усольцев Артём Денисович | АСОиУБ-23-2                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел, принадлежащих       *");
+            Console.WriteLine("* числовому отрезку [8, 17] количество всех делителей больше 8.           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Начало цикла: {startValue}");
+            Console.WriteLine($"Конец цикла: {stopValue}");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            Console.Write($"Количество делителей: {res}");
+            Console.ReadKey();
+        }
+    }
+}
